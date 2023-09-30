2da71e45bbc7a0ba4a1f25783709a86d7a741b0d
Death0Justice <Death_Justice@163.com>
2023-09-30 17:59:52 +0100

    Change date to col 2 and desc to col 3 ~ Change text wrap of desc ~ Add time of completing challenge ~ Add header to xlsx file ~ Change test file accordingly ~ Add test xlsx file

--- demand_queue.py
+++ demand_queue.py
@@ -1,6 +1,8 @@
 from collections import deque
 from functools import partial
 import sys, openpyxl
+from openpyxl import Workbook
+from openpyxl.styles import Alignment
 from datetime import datetime
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIntValidator, QIcon, QCloseEvent, QKeySequence, QCursor
@@ -26,12 +28,27 @@ class DemandQueue(QWidget):
         try:
             h = openpyxl.load_workbook('history.xlsx')
         except:
-            h = openpyxl.Workbook()
+            h = Workbook()
             h.active.title = '待完成点播'
+            h['待完成点播'].append([
+                "老板名称",
+                "点播时间",
+                "点播内容"
+            ])
+            h['待完成点播'].freeze_panes = 'A2'
             h.create_sheet('已完成点播')
+            h['已完成点播'].append([
+                "老板名称",
+                "点播时间",
+                "点播内容",
+                "完成时间"
+            ])
+            h['已完成点播'].freeze_panes = 'A2'
             h.save('history.xlsx')
         
-        for row in h['待完成点播'].values:
+        for idx, row in enumerate(h['待完成点播'].values):
+            if idx == 0:
+                continue
             self.queue.append(list(row))
         self.history_sheet = h['已完成点播']
         
@@ -73,8 +90,8 @@ class DemandQueue(QWidget):
         date_comp.setLayout(date_layout)
         
         edit_layout.addWidget(name_comp)
-        edit_layout.addWidget(desc_comp)
         edit_layout.addWidget(date_comp)
+        edit_layout.addWidget(desc_comp)
         edit.setLayout(edit_layout)
         
         insert = QWidget(self)
@@ -119,18 +136,18 @@ class DemandQueue(QWidget):
         n = len(queue)
         table.setRowCount(n)
         table.setColumnCount(3)
-        table.setHorizontalHeaderLabels(["老板名称", "点播内容", "点播时间"])
+        table.setHorizontalHeaderLabels(["老板名称", "点播时间", "点播内容"])
         table.horizontalHeader().setSectionResizeMode(0, QHeaderView.Interactive)
-        table.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
-        table.horizontalHeader().setSectionResizeMode(2, QHeaderView.Fixed)
-        table.setColumnWidth(2, 170)
+        table.horizontalHeader().setSectionResizeMode(1, QHeaderView.Fixed)
+        table.horizontalHeader().setSectionResizeMode(2, QHeaderView.Stretch)
+        table.setColumnWidth(1, 170)
         table.setContextMenuPolicy(Qt.CustomContextMenu)
         table.customContextMenuRequested.connect(self.contextMenu)
-        table.itemChanged.connect(self.updateHistory)
         for i in range(n):
             table.setItem(i, 0, QTableWidgetItem(queue[i][0]))
             table.setItem(i, 1, QTableWidgetItem(queue[i][1]))
             table.setItem(i, 2, QTableWidgetItem(queue[i][2]))
+        table.itemChanged.connect(self.updateHistory)
         return table
 
     def contextMenu(self):
@@ -169,6 +186,8 @@ class DemandQueue(QWidget):
         row = item.row()
         done = self.queue[row]
         self.history_sheet.append(done)
+        last = self.history_sheet.max_row
+        self.history_sheet[last][2].alignment = Alignment(wrap_text=True)
         del self.queue[row]
         self.table.removeRow(row)
         
@@ -187,10 +206,10 @@ class DemandQueue(QWidget):
         dialog.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         dialog_layout = QVBoxLayout()
         dialog_layout.addWidget(QLabel(f'{demand[0]}的点播'), alignment=Qt.AlignCenter)
-        info = QLabel(demand[1], dialog)
+        info = QLabel(demand[2], dialog)
         info.setWordWrap(True)
         dialog_layout.addWidget(info, alignment=Qt.AlignCenter)
-        dialog_layout.addWidget(QLabel(f'于{demand[2]}', dialog), alignment=Qt.AlignCenter)
+        dialog_layout.addWidget(QLabel(f'于{demand[1]}', dialog), alignment=Qt.AlignCenter)
         dialog.setLayout(dialog_layout)
         dialog.exec()
         
@@ -214,7 +233,11 @@ class DemandQueue(QWidget):
             # Can add pop-up here
             return
         done = self.queue.popleft()
+        now = datetime.now()
+        done.append(now.strftime(self.date_format))
         self.history_sheet.append(done)
+        last = self.history_sheet.max_row
+        self.history_sheet[last][2].alignment = Alignment(wrap_text=True)
         self.table.removeRow(0)
         
         self.editing = True
@@ -223,7 +246,7 @@ class DemandQueue(QWidget):
     def del_unformatted(self):
         if len(self.queue) == 0:
             return
-        while not self.validate(self.queue[-1][2]):
+        while self.queue and not self.validate(self.queue[-1][1]):
             self.queue.pop()
             self.table.removeRow(self.table.rowCount() - 1)
         self.editing = True
@@ -231,9 +254,9 @@ class DemandQueue(QWidget):
     
     def insert_queue(self, row, cut=False):
         name = self.name.toPlainText()
-        desc = self.desc.toPlainText()
         date = self.date.toPlainText()
-        new = [name, desc, date]
+        desc = self.desc.toPlainText()
+        new = [name, date, desc]
         if cut:
             # Pushing
             new = [new[0], "插播: " + new[1], new[2]]
@@ -255,9 +278,9 @@ class DemandQueue(QWidget):
         # self.updateXLSX()
         hint = "请输入以下格式点播：\n"
         hint += "[老板名称]\n"
-        hint += "[点播内容]\n"
         hint += "[点播时间]\n"
-        hint += "请用分号\";\"分隔多个点播\n"
+        hint += "[点播内容]\n"
+        hint += "请用半角分号\";\"分隔多个点播\n"
         hint += "请注意：多个点播不支持插播"
         text, ok = QInputDialog.getMultiLineText(self, "快速输入", hint)
 
@@ -270,45 +293,45 @@ class DemandQueue(QWidget):
                     row = [s for s in line.split("\n") if s]
                     # Appending
                     name = row[0]
-                    date = row[-1]
-                    desc = "\n".join(row[1:-1])
-                    self.queue.append([name, desc, date])
+                    date = row[1]
+                    desc = '\n'.join(row[2:])
+                    self.queue.append([name, date, desc])
                     n = self.table.rowCount()
                     self.table.insertRow(n)
                     self.table.setItem(n, 0, QTableWidgetItem(name))
-                    self.table.setItem(n, 1, QTableWidgetItem(desc))
-                    self.table.setItem(n, 2, QTableWidgetItem(date))
+                    self.table.setItem(n, 1, QTableWidgetItem(date))
+                    self.table.setItem(n, 2, QTableWidgetItem(desc))
             else:
                 row = [s for s in str(text).split("\n") if s]
                 name = row[0]
-                date = row[-1]
-                desc = "\n".join(row[1:-1])
+                date = row[1]
+                desc = "\n".join(row[2:])
                 self.name.setText(name)
-                self.desc.setText(desc)
                 self.date.setText(date)
+                self.desc.setText(desc)
         
         self.editing = True
         self.onlyInt.setTop(self.table.rowCount())
     
     def sort(self):
-        push_history = []
-        append_history = []
-        unformatted_history = []
+        cut_queue = []
+        normal_queue = []
+        unformatted_queue = []
         date_format = {}
         for h in self.queue:
-            date_format[h[2]] = self.validate(h[2])
+            date_format[h[1]] = self.validate(h[1])
             # print(date_format) if format != '' else print('None')
             if format:
-                if str(h[1]).startswith("插播"):
-                    push_history.append(h)
+                if str(h[2]).startswith("插播"):
+                    cut_queue.append(h)
                 else:
-                    append_history.append(h)
+                    normal_queue.append(h)
             else:
-                unformatted_history.append(h)
-        push_history.sort(key=lambda h: datetime.strptime(h[2], date_format[h[2]]), reverse=True)
-        append_history.sort(key=lambda h: datetime.strptime(h[2], date_format[h[2]]))
+                unformatted_queue.append(h)
+        cut_queue.sort(key=lambda h: datetime.strptime(h[1], date_format[h[1]]), reverse=True)
+        normal_queue.sort(key=lambda h: datetime.strptime(h[1], date_format[h[1]]))
         
-        self.queue = deque(push_history + append_history + unformatted_history)
+        self.queue = deque(cut_queue + normal_queue + unformatted_queue)
         # print(self.queue)
         self.table.clearContents()
         for i, h in enumerate(self.queue):
@@ -323,7 +346,7 @@ class DemandQueue(QWidget):
         conclusion.setWindowIcon(QIcon('isaac.ico'))
         conclusion.setWindowFlag(Qt.WindowContextHelpButtonHint, False)
         congrats = QLabel("排序完成！", conclusion)
-        result = QLabel(f'发现{len(unformatted_history)}个格式错误的点播。', conclusion)
+        result = QLabel(f'发现{len(unformatted_queue)}个格式错误的点播。', conclusion)
         conclusion_layout = QVBoxLayout()
         conclusion_layout.addWidget(congrats, alignment=Qt.AlignCenter)
         conclusion_layout.addWidget(result, alignment=Qt.AlignCenter)
@@ -337,8 +360,15 @@ class DemandQueue(QWidget):
         self.history_book.remove(queue)
         self.history_book.create_sheet('待完成点播', 0)
         queue = self.history_book['待完成点播']
-        for row in self.queue:
+        queue.append([
+            "老板名称",
+            "点播时间",
+            "点播内容"
+        ])
+        queue.freeze_panes = 'A2'
+        for idx, row in enumerate(self.queue):
             queue.append(row)
+            queue[idx + 2][2].alignment = Alignment(wrap_text=True)
         self.history_book.save('history.xlsx')
         self.editing = False
     
